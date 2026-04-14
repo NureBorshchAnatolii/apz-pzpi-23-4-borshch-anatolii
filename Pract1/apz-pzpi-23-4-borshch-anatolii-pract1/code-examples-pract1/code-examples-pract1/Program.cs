6e0a94cae8e60fc28f346ca0ff6be6951c97b7bf
NureBorshchAnatolii <anatolii.borshch@nure.ua>
2026-04-15 00:14:04 +0300

    Add files via upload

---
+++ Pract1/apz-pzpi-23-4-borshch-anatolii-pract1/code-examples-pract1/code-examples-pract1/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace CodeExamplesPract1;
+    
+public interface ICurrencySubscriber
+{ 
+ void Update(string currency, decimal rate);
+}
+
+public class MobileAppClient : ICurrencySubscriber
+{
+    public void Update(string currency, decimal rate)
+    {
+        Console.WriteLine($"[Mobile] New currency {currency}: {rate}");
+    }
+}
+
+public class WebDashboard : ICurrencySubscriber
+{
+    public void Update(string currency, decimal rate)
+    {
+        Console.WriteLine($"[Web] Updated chart for {currency}: {rate}");
+    }
+}
+
+public class AnalyticsService : ICurrencySubscriber
+{
+    public void Update(string currency, decimal rate)
+    {
+        Console.WriteLine($"[Analytics] Currency display {currency}: {rate} added to DB");
+    }
+}
+
+public class CurrencyRateService
+{
+    private List<ICurrencySubscriber> _subscribers = new();
+
+    public void Subscribe(ICurrencySubscriber subscriber)
+    {
+        _subscribers.Add(subscriber);
+    }
+
+    public void Unsubscribe(ICurrencySubscriber subscriber)
+    {
+        _subscribers.Remove(subscriber);
+    }
+
+    public void UpdateRate(string currency, decimal newRate)
+    {
+        Console.WriteLine($"\n[Service] Currency {currency} updated to {newRate}");
+        NotifySubscribers(currency, newRate);
+    }
+
+    private void NotifySubscribers(string currency, decimal rate)
+    {
+        foreach (var subscriber in _subscribers)
+        {
+            subscriber.Update(currency, rate);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var rateService = new CurrencyRateService();
+
+        var mobile = new MobileAppClient();
+        var web = new WebDashboard();
+        var analytics = new AnalyticsService();
+        
+        rateService.Subscribe(mobile);
+        rateService.Subscribe(web);
+        rateService.Subscribe(analytics);
+        
+        rateService.UpdateRate("USD", 41.25m);
+    }
+}
